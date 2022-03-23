b6f418bca2e1a2dcea2e3cd1075e8b90a543ed6e
frvalenzuela <frvalenzuela@uc.cl>
2022-03-23 11:14:00 -0300

    all3

--- index.js
+++ index.js
@@ -20,6 +20,20 @@ app.delete('/security', (req, res) => {
   res.status(401 ).send("Unauthorized");
 })
 
+app.get('//status', (req, res) => {
+
+  res.status(204 ).send("No content");
+})
+
+app.get('//info', (req, res) => {
+  let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  res.status(200).json({ url: fullUrl });
+})
+
+app.delete('//security', (req, res) => {
+  res.status(401 ).send("Unauthorized");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
